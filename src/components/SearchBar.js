26f708beb859dcdd104181e4c7fe728353a84346
OussamaAlouat <al317257@uji.es>
2019-01-09 07:56:27 +0100

    SearchBar created

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,28 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+
+    onInputChange = (event) => {
+
+    };
+
     render() {
-        return <div>SearchBar</div>
+        return (
+            <div className="search-bar ui segment">
+                <from className="ui form">
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            placeholder="Input the video"
+                            onChange={this.onInputChange}
+                        />
+                    </div>
+                </from>
+            </div>
+        );
     }
 }
 
