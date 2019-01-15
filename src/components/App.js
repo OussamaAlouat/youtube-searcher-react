dc67d3a9e40f21a278f0fbe982fa0eb2c65ba1e6
OussamaAlouat <al317257@uji.es>
2019-01-15 22:00:23 +0100

    Get the videos and set them at the app component state

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,27 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-    onTermSubmit = term => {
-        youtube.get('/search', {
+    state = {videos: []};
+
+    onTermSubmit = async term => {
+        const response = await youtube.get('/search', {
             params: {
-                q:term
+                q: term
             }
         });
+
+        this.setState({
+            videos: response.data.items
+        });
+
     };
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
+
+                
             </div>
         );
     }
