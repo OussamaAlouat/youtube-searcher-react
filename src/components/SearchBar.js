47ed1943e57c6a8c9dadc9bb7b13d02580ac066c
OussamaAlouat <al317257@uji.es>
2019-01-15 21:43:21 +0100

    Configuration of youtube API

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,7 +9,7 @@ class SearchBar extends React.Component {
 
     onFormSubmit = event => {
         event.preventDefault();
-        //ToDo Make suere we call callback from callback parent component
+        //ToDo: Make suere we call callback from callback parent component
     };
 
     render() {
