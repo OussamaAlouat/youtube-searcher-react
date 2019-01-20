1e25b99300538fd1358f499d92fd60c010f7466a
OussamaAlouat <al317257@uji.es>
2019-01-20 16:46:26 +0100

    Improve style and the aparience of video lists

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,15 @@
+import  './VideoItem.css';
 import React from 'react';
 
 const VideoItem = props => {
     return (
-        <div>
-            <img src={props.video.snippet.thumbnails.medium.url}/>
-            {props.video.snippet.title}
+        <div className="item video-item">
+            <img className="ui image" src={props.video.snippet.thumbnails.medium.url}/>
+            <div className="content">
+                <div className="header">
+                    {props.video.snippet.title}
+                </div>
+            </div>
         </div>
     );
 };
